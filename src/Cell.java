aee181861d7b2589f0afcb5ad122b78037735d5d
Ben Schwennesen <bas65@duke.edu>
2017-09-21 16:51:23 -0400

    Added cell abstract class (very basic, not close to done)

---
+++ src/Cell.java
@@ -0,0 +1,20 @@
+import javafx.geometry.Point2D;
+
+import java.util.List;
+
+public abstract class Cell {
+
+    private final List<Cell> neighbors;
+    /* ints come from X<ML for each simulation type?? */
+    private final Point2D gridLocation;
+    private int currentState;
+    private int nextState;
+
+    public Cell(int x, int y) {
+        neighbors = null;
+        gridLocation = new Point2D(x, y);
+    }
+
+    public abstract void updateState() ;
+
+}
