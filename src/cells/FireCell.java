7c1783add725db486bab123deb032efb014d401e
Ben Schwennesen <bas65@duke.edu>
2017-09-22 14:35:41 -0400

    Updated cell classes to account for the fact that they don't need to know their own position

--- src/cells/FireCell.java
+++ src/cells/FireCell.java
@@ -2,8 +2,8 @@ package cells;
 
 public class FireCell extends Cell {
 
-    public FireCell(int x, int y) {
-        super(x,y);
+    public FireCell() {
+        super();
     }
     public void calculateNextState() {
         // fill will rule checking for Spreading of Fire
