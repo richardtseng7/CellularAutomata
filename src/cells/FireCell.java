52de3f4e6c1c5f204f5dbb9be397e321ba064606
Ben Schwennesen <bas65@duke.edu>
2017-09-22 00:07:04 -0400

    Added classes for the four types of simulations to implement in the basic sprint; added the rules only in the Conway/Game of Life cell; others still need to have their rule logic added

---
+++ src/cells/FireCell.java
@@ -0,0 +1,11 @@
+package cells;
+
+public class FireCell extends Cell {
+
+    public FireCell(int x, int y) {
+        super(x,y);
+    }
+    public void calculateNextState() {
+        // fill will rule checking for Spreading of Fire
+    }
+}
---
+++ src/cells/WaTorCell.java
@@ -0,0 +1,11 @@
+package cells;
+
+public class WaTorCell extends Cell {
+
+    public WaTorCell(int x, int y) {
+        super(x,y);
+    }
+    public void calculateNextState() {
+        // fill will rule checking for Wa-Tor World
+    }
+}
