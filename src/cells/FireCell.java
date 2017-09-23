8f8663a7778b033b5586e3deb725f662ca47d742
Ben Schwennesen <bas65@duke.edu>
2017-09-22 22:16:24 -0400

    Add cell implementation for the spreading-fire simulation

--- src/cells/FireCell.java
+++ src/cells/FireCell.java
@@ -1,5 +1,10 @@
 package cells;
 
+import resources.PropertiesGetter;
+
+import java.util.Properties;
+import java.util.Random;
+
 public class FireCell extends Cell {
 
     private enum State {
@@ -8,11 +13,37 @@ public class FireCell extends Cell {
         EMPTY
     }
 
+    private double catchFireThreshold;
+
+    private Random catchFireDecisionMaker = new Random();
+
     public FireCell(Object initialState) {
+        catchFireThreshold = PropertiesGetter.getDefaultCatchFireProbability();
         super(initialState);
     }
 
     public void calculateNextState() {
-        // fill will rule checking for Spreading of Fire
+        State currentState = (State) getCurrentState();
+        if (currentState == State.TREE) {
+            for (Cell neighbor : getNeighbors()) {
+                if (neighbor.getCurrentState() == State.BURNING) {
+                    setNextState(catchFireOrNot());
+                }
+            }
+        } else if (currentState == State.BURNING) {
+            setNextState(State.EMPTY);
+        }
+    }
+
+    private State catchFireOrNot() {
+        if (catchFireDecisionMaker.nextDouble() < catchFireThreshold) {
+            return State.BURNING;
+        } else {
+            return State.TREE;
+        }
+    }
+
+    public void setCatchFireProbability(double catchFireProbability) {
+        this.catchFireProbability = catchFireProbability;
     }
 }
